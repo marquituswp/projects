3ae3547c665ca8ba3ec71ec3fe13d3d1ae0e5f26
marquituswp <marcos.lopez.ceb@immune.institute>
2024-05-29 13:29:25 +0200

    spiderman api

--- Mini-projects/API-moviesSpiderman/app.js
+++ Mini-projects/API-moviesSpiderman/app.js
@@ -4,19 +4,37 @@ async function fetchData(url, options) {
 		if (!response.ok) { // Verifica si la respuesta es correcta
 			throw new Error(`HTTP error! status: ${response.status}`);
 		}
-		const result = await response.json();
-		console.log(result);
+		const data = await response.json();
+		
+		arrayMovies = data.d;
+		arrayMovies.forEach(movie => {
+			const tittle = movie.l;
+			const image = movie.i.imageUrl;
+			const cast = movie.s;
+
+			const poster = 
+			`
+				<div>
+					<img src="${image}" alt="${tittle}">
+					<h2>${tittle}</h2>
+					<small>${cast}</small>
+				</div>
+			`
+			document.getElementById('container').innerHTML += poster;
+		});
+
+
 	} catch (error) {
 		console.error('Error occurred:', error);
 	}
 }
 
-const url = 'https://imdb-com.p.rapidapi.com/search?searchTerm=spiderman';
+const url = 'https://imdb8.p.rapidapi.com/auto-complete?q=spider';
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'fd1dc3f908msh9f8ab3414e4e3b8p1679bfjsn4618e9da5f04',
-		'X-RapidAPI-Host': 'imdb-com.p.rapidapi.com'
+		'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
 	}
 };
 
